8b70b9c87558fab28256b578f12080ae245ce49f
NateOwens <nathanieltylerowens@gmail.com>
2020-05-13 19:26:47 -0500

    added before merge

--- main.js
+++ main.js
@@ -260,6 +260,53 @@ const filterSweats = (event) => {
   
   buildProductCards(tempSweatsArr);
 }
+/*REVIEWS CODE */
+const reviews = [
+  {
+    name: "Jimmy Dean",
+    review: "The best sweatpants to enjoy sausage!",
+    publish: true
+  },
+  {
+    name: "Hodor",
+    review: "Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor",
+    publish: true
+  },
+  {
+    name: "Jon",
+    review: "I'm baby authentic PBR&B messenger bag neutra, put a bird on it keytar narwhal try-hard kitsch pitchfork chambray cardigan tumblr. Umami subway tile man braid copper mug tumeric readymade try-hard shoreditch tattooed kinfolk VHS williamsburg DIY keytar irony. YOLO truffaut seitan retro marfa shoreditch migas raw denim wolf actually helvetica. Ugh vaporware organic af VHS hella crucifix copper mug marfa. Farm-to-table austin 8-bit fingerstache. Semiotics art party shoreditch cliche, venmo brooklyn poutine readymade synth tbh squid.",
+    publish: true
+  },
+  {
+    name: "Lori Smith",
+    review: "Meh man braid franzen, eu austin irure in. Direct trade raclette poutine bushwick retro in. Fingerstache adaptogen activated charcoal velit nulla, pug aliqua direct trade. Meggings in raw denim, reprehenderit nulla hella ex.",
+    publish: true
+  },
+  {
+    name: "Craig Bunton",
+    review: "8-bit plaid beard tote bag blog blue bottle. Kitsch brunch kogi knausgaard chillwave art party woke leggings yuccie lumbersexual, vexillologist poutine. Copper mug narwhal aesthetic, occupy lo-fi XOXO gentrify migas vaporware tousled selvage adaptogen pour-over. Next level organic single-origin coffee pour-over.",
+    publish: true
+  },
+  {
+    name: "Carol Farrell",
+    review: "What's a rerun? You wanna a Pepsi, pall, you're gonna pay for it. Ah. There's that word again, heavy. Why are things so heavy in the future. Is there a problem with the Earth's gravitational pull? Just turn around, McFly, and walk away. Are you deaf, McFly? Close the door and beat it.",
+    publish: true
+  },
+];
+
+const buildReviews = () => {
+  let domString = '';
+
+  for (let i = 0; i < projects.length; i++) {
+    if (reviews[i].publish === true) {
+      domString += `<div class="reviewCards">`;
+    domString += `<h4 class="name">${reviews[i].name}</h4>`;
+    domString += `<p class="specificReviews">${reviews[i].review}</p>`;
+    domString += `</div>`;
+    }
+  }
+  printToDom('#', domString);
+}
 
 const clickEvents = () => {
   if (document.body.id === 'shop' || document.body.id === 'homepage') {
